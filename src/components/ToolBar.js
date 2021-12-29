f5e5d13ea44f028c0e53261d9bb3df8a5b3c2479
Damian Dalto <damianema21@gmail.com>
2021-12-29 20:50:49 -0300

    se decide el disenio de la ToolBar y se implementan eventos interactivos

--- src/components/ToolBar.js
+++ src/components/ToolBar.js
@@ -11,64 +11,66 @@ function ToolBar({setLineColor, setLineWidth}){
     
 
     return (
-        <div className="toolBar fixed-bottom">
-            <ButtonGroup id="toolbarBtnGroup">
-                <Button variant='secondary'>
-                    <Dropdown drop='up' autoClose='outside'>
-                        <Dropdown.Toggle variant='secondary'>
-                            Tools
-                        </Dropdown.Toggle>
+        <div className="toolBar fixed-bottom bg-secondary">
+            <Dropdown drop='up' autoClose='outside'>
+                <Dropdown.Toggle variant='secondary'>
+                    Tools
+                </Dropdown.Toggle>
 
-                        <Dropdown.Menu>
-                            <Dropdown.Item><button>pencil</button></Dropdown.Item>
-                            <Dropdown.Item><button>brush</button></Dropdown.Item>
-                            <Dropdown.Item><button>eraser</button></Dropdown.Item>
-                            <Dropdown.Item><button>filler</button></Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                </Button>
-                <Button variant='secondary'><input type="color" value="#000000"></input></Button>
-                <Button variant='secondary'>
-                    <Dropdown drop='up' autoClose='outside'>
-                        <Dropdown.Toggle variant='secondary'>
-                            Tool size
-                        </Dropdown.Toggle>
+                <Dropdown.Menu>
+                    <Dropdown.Item>pencil</Dropdown.Item>
+                    <Dropdown.Item>brush</Dropdown.Item>
+                    <Dropdown.Item>eraser</Dropdown.Item>
+                    <Dropdown.Item>filler</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+            <input type="color" 
+            value="#000000"
+            onChange={(e) => {
+                setLineColor(e.target.value);
+            }}
+            ></input>
+            <Dropdown drop='up' autoClose='outside'>
+                <Dropdown.Toggle variant='secondary'>
+                    Tool size
+                </Dropdown.Toggle>
 
-                        <Dropdown.Menu>
-                            <Dropdown.Item>
-                                <input type='range' min='1' max='40' ></input>
-                            </Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                </Button>
-                <Button variant='secondary'>
-                    <Dropdown drop='up' autoClose='outside'>
-                        <Dropdown.Toggle variant='secondary'>
-                            Layout
-                        </Dropdown.Toggle>
+                <Dropdown.Menu>
+                    <Dropdown.Item>
+                        <input type='range'
+                            min='1'
+                            max='40' 
+                            defaultValue='5'
+                            onChange={(e) => {
+                                setLineWidth(e.target.value);
+                            }}
+                            ></input>
+                    </Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+            <Dropdown drop='up' autoClose='outside'>
+                <Dropdown.Toggle variant='secondary'>
+                    Layout
+                </Dropdown.Toggle>
 
-                        <Dropdown.Menu>
-                            <Dropdown.Item>
-                                <button>gridOn</button>
-                            </Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                </Button>
-                <Button variant='secondary'>
-                    <Dropdown drop='up'>
-                        <Dropdown.Toggle variant='secondary'>
-                            Menu
-                        </Dropdown.Toggle>
+                <Dropdown.Menu>
+                    <Dropdown.Item>
+                        Grid On
+                    </Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+            <Dropdown drop='up'>
+                <Dropdown.Toggle variant='secondary'>
+                    Menu
+                </Dropdown.Toggle>
 
-                        <Dropdown.Menu>
-                            <Dropdown.Item>New...</Dropdown.Item>
-                            <Dropdown.Item>Open...</Dropdown.Item>
-                            <Dropdown.Item>Animation preview</Dropdown.Item>
-                            <Dropdown.Item>Help</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                </Button>
-            </ButtonGroup>
+                <Dropdown.Menu>
+                    <Dropdown.Item>New...</Dropdown.Item>
+                    <Dropdown.Item>Open...</Dropdown.Item>
+                    <Dropdown.Item>Animation preview</Dropdown.Item>
+                    <Dropdown.Item>Help</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
         </div>
     );
 }
