c85774b5677abdd96ca77588e656f7ba4858b540
Damian Dalto <damianema21@gmail.com>
2021-12-29 10:30:07 -0300

    se implementa la rejilla guia para el canvas

--- src/components/DrawingSpace.js
+++ src/components/DrawingSpace.js
@@ -5,66 +5,46 @@ import './assets/DrawingSpace.css';
 function DrawingSpace(){
     //atributos por default al iniciar el componente
     const canvasRef = useRef(null);
-    //const gridRef = useRef(null);
+    const gridRef = useRef(null);
     const canvasCtxRef = useRef(null);
-    //const gridCtxRef = useRef(null);
-    const [scale, setScale] = useState("20px");
-    //const [width, setWidth] = useState("320px");
-    //const [height, setHeight] = useState("320px");
+    const gridCtxRef = useRef(null);
     const [isDrawing, setIsDrawing] = useState(false);
     const [lineWidth, setLineWidth] = useState(5);
     const [lineColor, setLineColor] = useState("black");
-    const [lineOpacity, setLineOpacity] = useState(0.1);
 
     //inicializacion cuando el componente se monta por primera vez
     useEffect(() => {
         const canvas = canvasRef.current;
-        //canvas.width = width;
-        //canvas.height = height;
-        //canvasRef.current = canvas;
-
         const ctx = canvas.getContext("2d");
         ctx.lineCap = "round";
         ctx.lineJoin = "round";
-        ctx.globalAlpha = lineOpacity;
         ctx.strokeStyle = lineColor;
         ctx.lineWidth = lineWidth;
         canvasCtxRef.current = ctx;
-    }, [lineWidth, lineColor, lineOpacity]);
-/*
+    }, [lineWidth, lineColor]);
+
     useEffect(() => {
         const grid = gridRef.current;
-        grid.width = width;
-        grid.height = height;
-        gridRef.current = grid;
 
         const ctx = grid.getContext("2d");
         ctx.beginPath();
         ctx.strokeStyle = "black";
         ctx.lineWidth = 1;
-        for(var x = 0; x <= width; x += scale){
+        for(var x = 0; x <= canvasRef.current.width; x += 20){
 			ctx.moveTo(x, 0);
-			ctx.lineTo(x, height);
+			ctx.lineTo(x, canvasRef.current.width);
 		}
         ctx.stroke();
-        for(var y = 0; x <= height; y += scale){
-			ctx.moveTo(y, 0);
-			ctx.lineTo(y, width);
+        for(var y = 0; y <= canvasRef.current.height; y += 20){
+			ctx.moveTo(0, y);
+			ctx.lineTo(canvasRef.current.height, y);
 		}
         ctx.stroke();
         ctx.closePath();
 
         gridCtxRef.current = ctx;
-    }, [width, height, scale]);
-
-    const startCanvas = () => {
-        //canvasCtxRef.current.clearRect(0, 0, width, height);
-    };
-
-    const startGrid = () => {
-        //gridCtxRef.current.clearRect(0, 0, width, height);
-    };
-    */
+    }, []);
+    
 
     const startDrawing = (e) => {
         canvasCtxRef.current.beginPath();
@@ -94,15 +74,21 @@ function DrawingSpace(){
 
     return (
         <div className="drawingSpace">
-            <canvas 
+            <canvas id="mainCanvas"
             ref={canvasRef}
             width={`320px`} 
             height={`320px`}
-            onMouseDown={startDrawing}
-            onMouseUp={endDrawing}
-            onMouseMove={draw}
             />
             
+            <canvas id="gridCanvas"
+            ref={gridRef} 
+            width={`320px`}
+            height={`320px`}
+            onMouseDown={startDrawing}
+            onMouseUp={endDrawing}
+            onMouseOut={endDrawing}
+            onMouseMove={draw}
+            />
         </div>
     );
 }
