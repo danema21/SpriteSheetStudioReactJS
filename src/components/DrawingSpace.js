7a689dc56eadb2bab2ecf90e985486d510ed0a8a
Damian Dalto <damianema21@gmail.com>
2022-01-13 17:01:17 -0300

    completado prototipo de la herramienta de relleno, por el momento rellena toda la imagen con el color seleccionado

--- src/components/DrawingSpace.js
+++ src/components/DrawingSpace.js
@@ -12,7 +12,7 @@ function DrawingSpace(){
     const gridCtxRef = useRef(null);
     const [isDrawing, setIsDrawing] = useState(false);
     const [lineWidth, setLineWidth] = useState(1);
-    const [lineColor, setLineColor] = useState("black");
+    const [lineColor, setLineColor] = useState("#000000");
     const [tool, setTool] = useState("brush");
     const [gridOn, setGridOn] = useState(true);
     const [frameCount, setFrameCount] = useState(1);
@@ -99,54 +99,42 @@ function DrawingSpace(){
         setIsDrawing(true);
     };
 
-    const endDrawing = () => {
-        canvasCtxRef.current.closePath();
-        setIsDrawing(false);
-        recordAction();
-    };
-
-    const draw = (e) => {
-        if(!isDrawing){
-            return;
-        }
-
+    const endDrawing = (e) => {
         switch(tool){
-            case "brush":
-                if(e.touches == null){
-                    canvasCtxRef.current.lineTo(
-                        e.nativeEvent.offsetX,
-                        e.nativeEvent.offsetY
-                    );
-                }else{
-                    canvasCtxRef.current.lineTo(
-                        e.touches[0].clientX-canvasRef.current.offsetLeft,
-                        e.touches[0].clientY-canvasRef.current.offsetTop
-                    );
-                }
-                canvasCtxRef.current.stroke();
-                break;
-            case "pencil":
-                    canvasCtxRef.current.fillStyle = lineColor;
-                    if(e.touches == null){
-                        canvasCtxRef.current.fillRect(Math.floor(e.nativeEvent.offsetX / 20) * 20, Math.floor(e.nativeEvent.offsetY / 20) * 20, 20, 20);
-                    }else{
-                        let x = e.touches[0].clientX - canvasRef.current.offsetLeft;
-                        let y = e.touches[0].clientY - canvasRef.current.offsetTop;
-                        canvasCtxRef.current.fillRect(Math.floor(x / 20) * 20, Math.floor(y / 20) * 20, 20, 20);
-                    }
-                    break;
-            case "eraser":
-                    canvasCtxRef.current.fillStyle = "rgba(255, 255, 255, 255)";
-                    if(e.touches == null){
-                        canvasCtxRef.current.fillRect(e.nativeEvent.offsetX - (lineWidth/2), e.nativeEvent.offsetY - (lineWidth/2), lineWidth, lineWidth);
-                    }else{
-                        let x = e.touches[0].clientX - canvasRef.current.offsetLeft;
-                        let y = e.touches[0].clientY - canvasRef.current.offsetTop;
-                        canvasCtxRef.current.fillRect(x - (lineWidth/ 2), y - (lineWidth/ 2), 20, 20);
-                    }
-                    break;
             case "filler":
-                    /* falta implementar (complejidad muy alta)
+                let image = canvasCtxRef.current.getImageData(0, 0, canvasRef.current.width, canvasRef.current.height);
+                let pixelData = canvasCtxRef.current.getImageData(e.nativeEvent.offsetX, e.nativeEvent.offsetY, 1, 1).data;
+                let numPixels = image.data.length / 4;
+
+                const hexToRgbA = (hex) => {
+                    let c;
+                    if(/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)){
+                        c= hex.substring(1).split('');
+                        if(c.length === 3){
+                            c= [c[0], c[0], c[1], c[1], c[2], c[2]];
+                        }
+                        c= '0x'+c.join('');
+                        return 'rgba('+[(c>>16)&255, (c>>8)&255, c&255].join(',')+',1)';
+                    }
+                    throw new Error('Bad Hex');
+                }
+
+                let currentColor = hexToRgbA(lineColor).slice(5, hexToRgbA(lineColor).length -1);
+                let currentR = currentColor.split(',')[0];
+                let currentG = currentColor.split(',')[1];
+                let currentB = currentColor.split(',')[2];
+
+                for(let i = 0; i < numPixels; i++){
+                    image.data[i * 4] = currentR;
+                    image.data[i * 4 + 1] = currentG;
+                    image.data[i * 4 + 2] = currentB;
+                    image.data[i * 4 + 3] = 255;
+                }
+                
+                canvasCtxRef.current.putImageData(image, 0, 0);
+                break;
+
+                /* falta implementar (complejidad muy alta)
                     let pixelData = canvasCtxRef.current.getImageData(e.nativeEvent.offsetX, e.nativeEvent.offsetY, 1, 1).data;
                     let currentFillColor = lineColor.slice(5, lineColor.length-1);
                     let fillColorR = currentFillColor.split(',')[0];
@@ -220,9 +208,57 @@ function DrawingSpace(){
                             pixelPos += canvasRef.current.width * 4;
                         }
                     }
-                    canvasCtxRef.current.putImageData(colorLayer, 0, 0);*/
+                    canvasCtxRef.current.putImageData(colorLayer, 0, 0);
+                    */
+            default:
+                break;
+        }
+
+        canvasCtxRef.current.closePath();
+        setIsDrawing(false);
+        recordAction();
+    };
+
+    const draw = (e) => {
+        if(!isDrawing){
+            return;
+        }
+
+        switch(tool){
+            case "brush":
+                if(e.touches == null){
+                    canvasCtxRef.current.lineTo(
+                        e.nativeEvent.offsetX,
+                        e.nativeEvent.offsetY
+                    );
+                }else{
+                    canvasCtxRef.current.lineTo(
+                        e.touches[0].clientX-canvasRef.current.offsetLeft,
+                        e.touches[0].clientY-canvasRef.current.offsetTop
+                    );
+                }
+                canvasCtxRef.current.stroke();
+                break;
+            case "pencil":
+                    canvasCtxRef.current.fillStyle = lineColor;
+                    if(e.touches == null){
+                        canvasCtxRef.current.fillRect(Math.floor(e.nativeEvent.offsetX / 20) * 20, Math.floor(e.nativeEvent.offsetY / 20) * 20, 20, 20);
+                    }else{
+                        let x = e.touches[0].clientX - canvasRef.current.offsetLeft;
+                        let y = e.touches[0].clientY - canvasRef.current.offsetTop;
+                        canvasCtxRef.current.fillRect(Math.floor(x / 20) * 20, Math.floor(y / 20) * 20, 20, 20);
+                    }
                     break;
-                    
+            case "eraser":
+                    canvasCtxRef.current.fillStyle = "rgba(255, 255, 255, 255)";
+                    if(e.touches == null){
+                        canvasCtxRef.current.fillRect(e.nativeEvent.offsetX - (lineWidth/2), e.nativeEvent.offsetY - (lineWidth/2), lineWidth, lineWidth);
+                    }else{
+                        let x = e.touches[0].clientX - canvasRef.current.offsetLeft;
+                        let y = e.touches[0].clientY - canvasRef.current.offsetTop;
+                        canvasCtxRef.current.fillRect(x - (lineWidth/ 2), y - (lineWidth/ 2), 20, 20);
+                    }
+                    break;
             default:
                 break;
         }
